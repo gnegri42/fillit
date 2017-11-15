4076dfe158c828a57305423b88420d2e1d269ce5
Benjamin MUSELET <bmuselet@e3r11p10.42.fr>
2017-11-15 17:17:35 +0100

    enleve checker.c et ajout ft_checker.c

--- ft_checker.c
+++ ft_checker.c
@@ -1,19 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   checker.c                                          :+:      :+:    :+:   */
+/*   ft_checker.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bmuselet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/11/15 11:14:09 by bmuselet          #+#    #+#             */
-/*   Updated: 2017/11/15 13:22:28 by bmuselet         ###   ########.fr       */
+/*   Created: 2017/11/15 17:17:07 by bmuselet          #+#    #+#             */
+/*   Updated: 2017/11/15 17:17:11 by bmuselet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 #include "libft/libft.h"
 
-int	checker(char *str)
+int	ft_checker(char *str)
 {
 	if (!str)
 		return (0);
